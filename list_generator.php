b9f45b4d25e6436d90902da302a575c45426b868
Sunegi <85598386+Sunegi@users.noreply.github.com>
2021-06-29 23:55:48 +0100

    Update list_generator.php

--- list_generator.php
+++ list_generator.php
@@ -2,7 +2,7 @@
   header('Content-Type: text/plain; charset=UTF-8');
   echo '[iThreads Ghoster]'.PHP_EOL;
   echo '! Title: iThreads Ghoster'.PHP_EOL;
-  echo '! Expires: 2 hours'.PHP_EOL.PHP_EOL;
+  echo '! Expires: 1 hour'.PHP_EOL.PHP_EOL;
   foreach(file('threads.txt') as $line) {
     echo 'ivelt.com#?#LI:-abp-has(>DL>DT>DIV>A:-abp-contains('.trim($line).'))'.PHP_EOL;
   }
